06688531fec89b00fd04c3886c0d947e9dd29d0e
Juan Pablo <juanpan2409@icloud.com>
2022-12-21 19:11:29 -0400

    subiendo

---
+++ index.js
@@ -0,0 +1,50 @@
+
+const express = require('express')
+const app = express()
+let notes = [
+    {
+        "id": 1,
+        "content": "Tengo que estudiar NODE JS",
+        "date": "2019-05-30T17:30:31.098Z",
+        "isImportant": true
+    },
+    {
+        "id": 2,
+        "content": "Tengo que estudiar REACT",
+        "date": "2019-05-30T17:30:31.098Z",
+        "isImportant": true
+    },
+    {
+        "id": 3,
+        "content": "Tengo que estudiar ENGLISH",
+        "date": "2019-05-30T17:30:31.098Z",
+        "isImportant": true
+    },
+    {
+        "id": 4,
+        "content": "Tengo que descansar",
+        "date": "2019-05-30T17:30:31.098Z",
+        "isImportant": false
+    }
+]
+
+
+
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'text/plain'})
+//     response.end("Hi my little friend")
+
+// })
+
+app.get('/', (request, response)=>{
+    response.send('<h1>hola amigos<h1>')
+})
+
+app.get('/api', (request, response) => {
+    response.json(notes)
+})
+
+const PORT = 3001
+app.listen( PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
